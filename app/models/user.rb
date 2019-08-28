ad2ead55c5d4cdecda259e1a15b696463f7db1f6
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-08-28 10:00:38 +0000

    Insta

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 40}
-    validates :email, presence: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :email, presence: true, uniqueness: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     before_validation { email.downcase! }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
     has_one :profile
     has_many :posts
-    has_many :favorites, dependent: :destroy
-   
-   
+    has_many :favorites, dependent: :destroy 
 end
